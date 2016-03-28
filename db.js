be494b8abd3893f6f7c8b879b89ce899156bb3d8
Ammon S <ammons@qualtrics.com>
2016-03-28 15:44:36 -0600

    switching to bluebird

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-var mongoClient = require('mongodb').MongoClient
-var q = require('q');
+var mongoClient = require('mongo').MongoClient
+var Promise = require('bluebird');
 
 var state = {
 	db: null,
@@ -7,112 +7,68 @@ var state = {
 
 var exports = module.exports = {};
 
+function afterConnect(db,collections,resolve){
+	state.db = db
+	for (var i in config.collections) {
+		exports[collections[i]] = Promise.promisifyAll(state.db.collection(collections[i]))
+	}
+	resolve("Successfully connected to mongo");
+}
+
 exports.connect = function(config) {
-	var deferred = q.defer();
-
-	if (state.db){
-		deferred.resolve("Already connected to mongo");
-	}
-
-	var url = config.host + "/" + config.db;
-	if(config.replicaSet){
-		url += "?replicaSet=" + config.replicaSet;
-	}
-	mongoClient.connect(url, function(err, db) {
-		if (err){
-			deferred.reject(err)
+	return new Promise(function(resolve,reject){
+		if (state.db){
+			resolve("Already connected to mongo");
 		}
-		else{
-			if(config.auth){
-				db.admin().authenticate(config.auth.username,config.auth.password,function(err,res){
-					if(err){
-						deferred.reject(err);
-					}
-					else{
-						state.db = db
-						deferred.resolve("Successfully connected to mongo");
-					}
-				})
+
+		var url = config.host + "/" + config.db;
+		if(config.replicaSet){
+			url += "?replicaSet=" + config.replicaSet;
+		}
+		mongoClient.connect(url, function(err, db) {
+			if (err){
+				reject(err)
 			}
 			else{
-				state.db = db
-				deferred.resolve("Successfully connected to mongo");
+				if(config.auth){
+					db.admin().authenticate(config.auth.username,config.auth.password,function(err,res){
+						if(err){
+							reject(err);
+						}
+						else{
+							afterConnect(db,config.collections,resolve);
+						}
+					})
+				}
+				else{
+					afterConnect(db,config.collections,resolve);
+				}
 			}
-		}
-	})
-
-	return deferred.promise;
+		});
+	});
 }
 
 exports.close = function() {
-	var deferred = q.defer();
-	if (state.db) {
-		state.db.close(function(err, result) {
-			if(err){
-				deferred.reject(err);
-			}
-			else{
-				state.db = null
-				deferred.resolve("Successfully closed mongo connection");
-			}
-		})
-	}
-	else{
-		deferred.resolve("Mongo connection already closed");
-	}
-	return deferred.promise;
+	return new Promise(function (resolve,reject){
+		if (state.db) {
+			state.db.close(function(err, result) {
+				if(err){
+					reject(err);
+				}
+				else{
+					state.db = null
+					resolve("Successfully closed mongo connection");
+				}
+			})
+		}
+		else{
+			resolve("Mongo connection already closed");
+		}
+	});
 }
 
 exports.db = function() {
 	return {
 		db: state.db
-	}
-}
-
-exports.collection = function(collectioName){
-	return {
-		collection: state.db.collection(collectioName),
-
-		findOne: function(match){
-			var deferred = q.defer();
-			this.collection.find(match).limit(1).next(done(deferred));
-			return deferred.promise;
-		},
-
-		find: function(match){
-			var deferred = q.defer();
-			this.collection.find(match).toArray(done(deferred));
-			return deferred.promise;
-		},
-
-		aggregate: function(pipeline){
-			var deferred = q.defer();
-			this.collection.aggregate(pipeline,done(deferred));
-			return deferred.promise;
-		},
-
-		update: function(match,updateObj,options){
-			var deferred = q.defer();
-			this.collection.update(match,updateObj,options,done(deferred));
-			return deferred.promise;
-		},
-
-		remove: function(match){
-			var deferred = q.defer();
-			this.collection.remove(match,options,done(deferred));
-			return deferred.promise;
-		}
-	}
-
-}
-
-function done(deferred){
-	return function(err,result){
-		if(err){
-			deferred.reject(err);
-		}
-		else{
-			deferred.resolve(result);
-		}
 	}
 }
